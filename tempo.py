ba45a8a940d3ac47c52cc9fc25da442c519b7db0
thaynar96 <thaynarbo@gmail.com>
2021-07-08 22:01:01 -0300

    alterei nome dos atributos e adicionei métodos privados

--- tempo.py
+++ tempo.py
@@ -1,10 +1,32 @@
 class Tempo:
     def __init__(self):
-        self.hora = 5
-        self.minuto = 0
+        self.__hour = 5
+        self.__minute = 0 
 
-    def passarTempo(self, minutos, hora=5):
-        self.minuto += minutos
-        while self.minuto >= 60:
-            self.minuto -= 60
-            self.hora += 1
+    def __str__(self):
+        return f'São {self.__hour} : {self.__minute}'
+
+    @property
+    def hour(self):
+        return self.__hour
+
+    @hour.setter
+    def hour(self, hour):
+        self.__hour = hour
+
+    @property
+    def minute(self):
+        return self.__minute
+
+    @minute.setter
+    def minute(self, minute):
+        self.__minute = minute
+
+
+    def passarTempo(self, minutes, hour=5):
+        self.minute += minutes
+        while self.minute >= 60:
+            self.minute -= 60
+            self.hour += 1
+
+print(Tempo())
